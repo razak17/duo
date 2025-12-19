14bfcdb088ce228a5d35cbcded72f470d34371ee
Abdul Razak <moabdulrazak17@gmail.com>
2025-12-19 19:15:18 +0000

    feat(marketing): landing page

--- src/routes/(marketing)/index.tsx
+++ src/routes/(marketing)/index.tsx
@@ -1,9 +1,59 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from '@clerk/clerk-react'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Loader } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/(marketing)/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/(marketing)/"!</div>
+  return (
+    <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
+      <div className="mb-8 h-[240px] w-[240px] lg:mb-0 lg:h-[424px] lg:w-[424px]">
+        <img
+          src="/hero.svg"
+          alt="Hero"
+          className="h-full w-full object-contain"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="max-w-[480px] text-center font-bold text-neutral-600 text-xl lg:text-3xl">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+        <div className="flex w-full max-w-[330px] flex-col items-center gap-y-3">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link to="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  )
 }
