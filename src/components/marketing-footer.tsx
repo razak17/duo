983fd2fd95ba832c2930b4c87d412ca3b976b5ab
Abdul Razak <moabdulrazak17@gmail.com>
2025-12-19 19:41:18 +0000

    refactor(marketing-footer): style properly

--- src/components/marketing-footer.tsx
+++ src/components/marketing-footer.tsx
@@ -1,59 +1,28 @@
 import { Button } from '@/components/ui/button'
 
+const languages = [
+  { src: '/es.svg', label: 'Spanish' },
+  { src: '/de.svg', label: 'German' },
+  { src: '/it.svg', label: 'Italian' },
+  { src: '/jp.svg', label: 'Japanese' },
+]
+
 export function MarketingFooter() {
   return (
     <footer className="hidden h-20 w-full border-slate-200 border-t-2 p-2 lg:block">
       <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
-        <Button size="lg" variant="ghost" className="w-full">
-          <img
-            src="/hr.svg"
-            alt="Croatian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Croatian
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <img
-            src="/es.svg"
-            alt="Spanish"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Spanish
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <img
-            src="/fr.svg"
-            alt="French"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          French
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <img
-            src="/it.svg"
-            alt="Italian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Italian
-        </Button>
-        <Button size="lg" variant="ghost" className="w-full">
-          <img
-            src="/jp.svg"
-            alt="Japanese"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Japanese
-        </Button>
+        {languages.map(({ src, label }) => (
+          <Button key={label} size="lg" variant="ghost">
+            <img
+              src={src}
+              alt={label}
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            {label}
+          </Button>
+        ))}
       </div>
     </footer>
   )
