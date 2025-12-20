8146b48b1be22da37dd44504bbc2f948bcb91f69
Abdul Razak <moabdulrazak17@gmail.com>
2025-12-20 07:34:06 +0000

    chore(todo): remove todos code

--- src/features/todo/constants.ts
+++
@@ -1,4 +0,0 @@
-export const TODOS_QUERY_KEYS = {
-  all: ['todos'],
-  byId: (id: number) => [TODOS_QUERY_KEYS.all, id],
-} as const
--- src/features/todo/server/data-access.ts
+++
@@ -1,7 +0,0 @@
-import { db } from '@/lib/db'
-import { todos } from '@/lib/db/schema/todos'
-
-export async function getTodos() {
-  const result = await db.select().from(todos).orderBy(todos.createdAt)
-  return result
-}
