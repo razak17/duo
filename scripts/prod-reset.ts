d4f00c683af4a927025e0db5050ec24cef88f080
Abdul Razak <moabdulrazak17@gmail.com>
2025-12-24 19:45:34 +0000

    feat(scripts): add prod reset script

--- scripts/prod-reset.ts
+++ scripts/prod-reset.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from 'src/lib/db/schema'
 
-config({ path: ['.env.local'] })
+config({ path: ['.env'] })
 
 // biome-ignore lint/style/noNonNullAssertion: ignore
 const sql = neon(process.env.DATABASE_URL!)
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from 'src/lib/db/schema'
 
-config({ path: ['.env.local', '.env'] })
+config({ path: ['.env.local'] })
 
 // biome-ignore lint/style/noNonNullAssertion: ignore
 const sql = neon(process.env.DATABASE_URL!)
