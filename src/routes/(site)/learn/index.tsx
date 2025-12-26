65b088995cfb7f3c9b9ac0a2caf8eed7ca9e70f2
Abdul Razak <moabdulrazak17@gmail.com>
2025-12-26 00:48:25 +0000

    fix(pages): center loader properly

--- src/routes/(site)/learn/index.tsx
+++ src/routes/(site)/learn/index.tsx
@@ -113,7 +113,7 @@ function RouteComponent() {
 
 function PendingComponent() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <Loader className="h-6 w-6 animate-spin text-muted-foreground" />
     </div>
   )
